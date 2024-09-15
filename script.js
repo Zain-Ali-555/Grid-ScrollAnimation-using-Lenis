0522c01cc54aa6c4ff80db35a255d1a8217325a8
MalikZain555 <zainchamp5@gmail.com>
2024-09-15 18:32:03 +0500

    changes in main Folder
    Added code in Git Folder

---
+++ script.js
@@ -0,0 +1,47 @@
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+document.querySelectorAll(".elem").forEach((elem) => {
+  let image = elem.querySelector("img");
+  let tl = gsap.timeline();
+
+  let xTransform = gsap.utils.random(-100, 100);
+
+  tl.set(
+    image,
+    {
+      transformOrigin: `${xTransform < 0 ? 0 : "100%"} `,
+    },
+    "start"
+  )
+    .to(
+      image,
+      {
+        scale: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: elem,
+          start: "top top",
+          end: "bottom top",
+          scrub: 1,
+        },
+      },
+      "start"
+    )
+    .to(image, {
+      xPercent: xTransform,
+      ease: "none",
+      scrollTrigger: {
+        trigger: elem,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
+});
